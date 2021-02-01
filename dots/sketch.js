f0035d11e8e9b2bf993c35dae90386220c3fdb8b
Andrew Kleindolph <akleindolph@gmail.com>
2021-01-31 18:15:29 -0800

    Update sketch.js

--- dots/sketch.js
+++ dots/sketch.js
@@ -1,14 +1,23 @@
-var x
-var y
-var s
-var c
-var r
-var g
-var b
+let x
+let y
+let s
+let c
+let r
+let g
+let b
+let currentMillis
+let previousMillis = 0
+let interval = 2000
+let pick = 0
+
+let words =['squid','flounder','clam','krill','prawn']
+
 
 function setup() {
   createCanvas(800, 480);
   background(0);
+  textSize(32);
+  textAlign(RIGHT);
 }
 
 function draw() {
@@ -20,9 +29,20 @@ function draw() {
     fill(c);
     ellipse(x,y,s,s);
   }
+  if (currentMillis>(previousMillis+interval)){
+    pick = random(0,words.length)
+    fill(255)
+    text(words[int(pick)],x,y)
+    previousMillis = currentMillis;
+    textSize(random(24,200));
+  }
+  
+  currentMillis=millis()
+  
   r=random(0,100);
   g=random(0,255);
   b=random(0,255);
   fill(r,g,b);
   ellipse(x,y,s,s);
+  
 }
