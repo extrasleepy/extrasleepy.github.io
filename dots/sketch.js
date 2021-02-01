18fe7c2c85f86c2f813115782b5afcfed023f90a
Andrew Kleindolph <akleindolph@gmail.com>
2021-01-31 17:29:06 -0800

    Update sketch.js

--- dots/sketch.js
+++ dots/sketch.js
@@ -1,7 +1,10 @@
 var x
 var y
-var r
+var s
 var c
+var r
+var g
+var b
 
 function setup() {
   createCanvas(800, 480);
@@ -9,10 +12,17 @@ function setup() {
 }
 
 function draw() {
-  x=random(0,width);
-  y=random(0,height);
-  r=random(2,10);
-  c=random(0,255);
-  fill(c);
-  ellipse(x,y,r,r);
+  for (let i = 0; i < 9; i++) {
+    x=random(0,width);
+    y=random(0,height);
+    s=random(2,10);
+    c=random(0,255);
+    fill(c);
+    ellipse(x,y,s,s);
+  }
+  r=random(0,100);
+  g=random(0,255);
+  b=random(0,255);
+  fill(r,g,b);
+  ellipse(x,y,s,s);
 }
